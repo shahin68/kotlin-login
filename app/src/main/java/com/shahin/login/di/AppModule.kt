9e7494373fa99a2391f6aa997ae75f112e6560f3
shahin68 <shahin.montazeri@gmail.com>
2021-03-01 11:13:40 +0330

    main frag added

--- app/src/main/java/com/shahin/login/di/AppModule.kt
+++ app/src/main/java/com/shahin/login/di/AppModule.kt
@@ -3,11 +3,13 @@ package com.shahin.login.di
 import com.shahin.login.data.sources.Repository
 import com.shahin.login.data.sources.RepositoryImpl
 import com.shahin.login.ui.fragments.login.LoginViewModel
+import com.shahin.login.ui.fragments.main.MainViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
     viewModel { LoginViewModel(get()) }
+    viewModel { MainViewModel() }
 
     factory<Repository> { RepositoryImpl() }
 }
